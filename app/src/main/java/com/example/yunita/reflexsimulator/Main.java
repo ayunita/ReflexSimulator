1ba3d3c2de24efa4699f3168e625e47aadb82d7c
ayunita <yunita@ualberta.ca>
2015-09-17 23:33:36 -0600

    File sync to home system.

--- app/src/main/java/com/example/yunita/reflexsimulator/Main.java
+++ app/src/main/java/com/example/yunita/reflexsimulator/Main.java
@@ -1,9 +1,11 @@
 package com.example.yunita.reflexsimulator;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Main extends ActionBarActivity {
 
@@ -33,5 +35,10 @@ public class Main extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void gotoSingleMode(View view){
+        Intent intent = new Intent(this, SinglePlayer.class);
+        startActivity(intent);
     }
 }
