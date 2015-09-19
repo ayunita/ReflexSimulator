cd77670e92cb5f805ff6b273e2d3f98ec7750cc0
ayunita <yunita@ualberta.ca>
2015-09-18 22:06:09 -0600

    Implementing main menu.

--- app/src/main/java/com/example/yunita/reflexsimulator/Main.java
+++ app/src/main/java/com/example/yunita/reflexsimulator/Main.java
@@ -1,25 +1,24 @@
 package com.example.yunita.reflexsimulator;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class Main extends ActionBarActivity {
+public class Main extends AppCompatActivity {
+
+    private final static String ICON = "heydings_icons.ttf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        setMainLayout();
     }
 
     @Override
@@ -37,8 +36,29 @@ public class Main extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void setMainLayout(){
+        Typeface icon_font = Typeface.createFromAsset(getAssets(), ICON);
+        TextView icon1 = (TextView) findViewById(R.id.reaction_timer_icon);
+        TextView icon2 = (TextView) findViewById(R.id.gameshow_icon);
+        TextView icon3 = (TextView) findViewById(R.id.statistic_icon);
+
+        icon1.setTypeface(icon_font);
+        icon2.setTypeface(icon_font);
+        icon3.setTypeface(icon_font);
+    }
+
     public void gotoSingleMode(View view){
-        Intent intent = new Intent(this, SinglePlayer.class);
+        Intent intent = new Intent(this, ReactionTimer.class);
+        startActivity(intent);
+    }
+
+    public void gotoPartyMode(View view){
+        Intent intent = new Intent(this, Gameshow.class);
+        startActivity(intent);
+    }
+
+    public void gotoStatistic(View view){
+        Intent intent = new Intent(this, Statistic.class);
         startActivity(intent);
     }
 }
